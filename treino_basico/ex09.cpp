f24c63652601974027ea4194afb0b98d8f7dec20
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2023-03-29 21:26:26 -0400

    updated

--- treino_basico/ex09.cpp
+++ treino_basico/ex09.cpp
@@ -3,8 +3,17 @@
 
 using namespace std;
 
+        //para deixar um paramtro por default em c++, colocamos o valor default no prototipo da função
+void imp(string t="não houve parametros");
+
 int main(int argc, char *argv[]){
+
+    imp("ola mundo");
 
     cout << endl;
     return 0;
+}
+
+void imp(string txt){
+    cout << "\n" << txt << "\n";
 }
