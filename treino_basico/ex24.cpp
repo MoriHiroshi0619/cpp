7430aca01552dda8cfc2a2e3c8c59a94face9ae2
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2023-04-07 22:45:44 -0400

    updated

---
+++ treino_basico/ex24.cpp
@@ -0,0 +1,13 @@
+//aprendendo a trabalhar com system
+#include <iostream>
+#include <stdlib.h> // para não ter erros com system();
+
+using namespace std;
+
+int main (int argc, char*argv[]){
+    system("clear");
+    system("ls");
+
+    cout << endl;
+    return 0;
+}
