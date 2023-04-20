c24f99d1d75367fda82f390987077c8f09aeb896
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2023-04-20 11:51:04 -0400

    updated

--- POO/aula02/classes.h
+++ POO/aula02/classes.h
@@ -3,29 +3,65 @@
 
 class Veiculo{
 public:
-    int velMax;
     int tipo;
+    int vel;
+
     Veiculo(int tp);
+    int getVelMax();
+    std::string getNome(); 
+    bool isLigado();
+    void setLigado(bool);
+    void status();
 private:
     std::string nome;
-    int velAtual;
+    int velMax;
     bool ligado;
 
+    void setVelMax(int);
+    void setNome(std::string);
 };
 //tipo 1 = carro | tipo 2 = aviao | tipo 3 = navio
 Veiculo::Veiculo(int tp){
-    tipo = tp;
+    this->tipo = tp;
+    setLigado(false);
     if(tipo == 1){
-        nome = "carro";
-        velMax = 200;
+        setNome("carro");
+        setVelMax(200);
     }else if(tipo == 2){
-        nome = "avião";
-        velMax = 800;
+        setNome("avião");
+        setVelMax(800);
     }else if(tipo == 3){
-        nome = "navio";
-        velMax = 120;
+        setNome("navio");
+        setVelMax(120);
     }
 }
 
-
+int Veiculo::getVelMax(){
+    return this->velMax;
+}
+void Veiculo::setVelMax(int vm){
+    this->velMax = vm;
+}
+std::string Veiculo::getNome(){
+    return this->nome;
+}
+void Veiculo::setNome(std::string n){
+    this->nome = n;
+}
+bool Veiculo::isLigado(){
+    return this->ligado;
+}
+void Veiculo::setLigado(bool l){
+    this->ligado = l;
+}
+void Veiculo::status(){
+    std::cout << std::endl;
+    std::cout << "Nome do veiculo             : " << this->nome << std::endl;
+    std::cout << "Velocidade maxima do veiculo: " << this->velMax << std::endl;
+    if(this->ligado){
+        std::cout << "Veiculo está ligado !: " << std::endl;
+    }else{
+        std::cout << "Veiculo está desligado !: " << std::endl;
+    }
+}
 #endif
--- POO/aula02/main.cpp
+++ POO/aula02/main.cpp
@@ -5,7 +5,14 @@ using namespace std;
 int main(int argc, char *argv[]){
     system("clear");
     Veiculo *v1 = new Veiculo(2);
-    cout << v1->tipo;
+    Veiculo *v2 = new Veiculo(1);
+    Veiculo *v3 = new Veiculo(3);
+
+    v1->status();
+    v2->status();
+    v3->setLigado(true);
+    v3->status();
+
     cout << endl;
     return 0;
 }
